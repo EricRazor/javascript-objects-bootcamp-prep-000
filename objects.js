dbd9da40a6a2d4174f0b53b30baf9e8f45179bce
Eric Razor <eric.razorjr@gmail.com>
2018-02-10 04:51:36 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var meals = {breakfast: "oatmeal",
+             lunch: "eggs"
+             }; //literal
+             
+
+var playlist =  [{hippieSabotage: "your soul"},{travisScott: "black & chinese"}]
+function updatePlaylist(playlist,artistName,songTitle){
+  
+}
